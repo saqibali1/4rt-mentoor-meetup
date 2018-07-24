7457621e853fdc0f2705c365152b1932a9e284b1
saqibali1 <38105428+saqibali1@users.noreply.github.com>
2018-07-24 15:57:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+ let jobTitle= `Manager`;
+ let geoLocation =  `Pakistan`;
+ let favCar = `Toyota corolla`;
+ 
+ console.log(`you will be   ${jobTitle}  in ${geoLocation}  and you you drive   ${favCar}`);
